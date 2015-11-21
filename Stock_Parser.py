5792585f508d1afb2c51c26894fbcc7c83156b93
kyxsune <kyxsune@gmail.com>
2015-11-21 01:01:32 -0500

    "Adjusted the Parser to take in a normal list

--- Stock_Parser.py
+++ Stock_Parser.py
@@ -14,16 +14,19 @@ def stock_get(Symbols):
     if not type(Symbols) is list: #Make sure Symbols is a list (if not raise an error)
         raise TypeError("Must be a list of the Stock Symbols")
     # Create URL from Components (used in line for loop and reduce to simplify
-    Base_url = 'https://query.yahooapis.com/v1/public/yql?q=select * '
+    base_url = 'https://query.yahooapis.com/v1/public/yql?q=select * '
     query = 'from yahoo.finance.quote where symbol in '
     sub = [('{' + str(x) + '},') for x in range(len(Symbols))]
-    query_full = (query + '(' + reduce(concat,sub)[:-1] + ')').format(*Symbols)
-    url_full = Base_url + quote_plus(query_full) + '&format=json&env=http://datatables.org/alltables.env' # This last part is a workaround due to Yahoo error stuff
+    query_full = (query + '(' + '"' + reduce(concat,sub)[:-1] + '"' + ')').format(*Symbols)
+    url_full = base_url + quote_plus(query_full) + '&format=json&env=http://datatables.org/alltables.env' # This last part is a workaround due to Yahoo error stuff
     # Open and load json into dictionary used simplejson and urllib
     result = load(urlopen(url_full))
     return result
 
-j = ['"GOOG"','"APPL"','"SNE"']
+j = ["GOOG", "APPL", "SNE"]
 
 print stock_get(j)['query']['results']['quote']
+
+print stock_get(j)
+
 
