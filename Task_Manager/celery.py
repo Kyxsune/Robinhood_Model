537da367e2b2c64cc58a6fd9fc48eddb43f6ec14
kyxsune <twintails0@gmail.com>
2015-12-20 01:20:00 -0500

    Testing the old fashion way first

--- Task_Manager/celery.py
+++ Task_Manager/celery.py
@@ -1,11 +1,11 @@
 #This is the file for the celery app
 from __future__ import absolute_import
-
+from Task_Manager import config
 from celery import Celery
 
 app = Celery()
 
-app.config_from_object('celeryconfig')
+app.config_from_object(config)
 
 if __name__ == '__main__':
     app.start()
