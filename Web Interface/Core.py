52488a37c0c1349bf6c6a1c43ef476070166aa62
kyxsune <twintails0@gmail.com>
2015-12-24 03:54:36 -0500

    Have to learn more about flask, but pain med dulling mind. Should not be coding day after surgery

--- Web Interface/Core.py
+++ Web Interface/Core.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask, request , render_template , redirect , url_for
 from pymongo import MongoClient
 from flask_bootstrap import Bootstrap
 
@@ -14,12 +14,24 @@ app = create_app()
 # Define Routes
 @app.route('/')
 def hello_world():
-    return render_template('add.html')
+    return "Bokeh Server goes here"
+
+
+@app.route('/result', methods=['POST'])
+def it_worked():
+    return "It worked"
+
 
 @app.route('/stock_add', methods=['GET','POST'])
 def stock_interest():
     if request.method == 'GET':
-        pass
+        return render_template('add.html')
+    if request.method == 'POST':
+        sl = MongoClient()
+        post = { "_id" : str(request.form['Symbol']) }
+        sl.stock_list.insert_one(post)
+        MongoClient().close()
+        return redirect(url_for('it_worked'))
 
 # Run Application
 if __name__ == '__main__':
