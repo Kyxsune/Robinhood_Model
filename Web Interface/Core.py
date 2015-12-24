3a87e01fa63e23a782d97cd7ded3a41909033ee7
kyxsune <twintails0@gmail.com>
2015-12-24 03:30:54 -0500

    Starting on the web interface using bootstrap cause html is not my forte

--- Web Interface/Core.py
+++ Web Interface/Core.py
@@ -1,18 +1,26 @@
 from flask import *
 from pymongo import MongoClient
+from flask_bootstrap import Bootstrap
 
 #Define Application and Database
-app = Flask(__name__)
-app.debug = True
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+    Bootstrap(app)
+    return app
 
+app = create_app()
+
+# Define Routes
 @app.route('/')
 def hello_world():
-    return 'Bokeh Goes Here'
+    return render_template('add.html')
 
 @app.route('/stock_add', methods=['GET','POST'])
 def stock_interest():
     if request.method == 'GET':
         pass
 
+# Run Application
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
